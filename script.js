9ddbd8193950b8ab09e4031bff6c2e90c8e979aa
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-02-19 15:33:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 setCanvas(document.getElementById("game"));
 size(innerWidth, innerHeight);
 
-var Node = function(x, y) {
+var Node = function(x, y, r) {
     this.pos = new DVector(x, y);
     this.vel = new DVector(0, 0);
-    this.mass = random(20, 30);
+    this.r = r;
+    this.mass = PI * sq(r) * 0.2;
 };
 Node.prototype.update = function() {
     this.vel.y += this.mass * dt;
     this.vel.mult(0.98)
     this.pos.add(this.vel);
 
-    if(this.pos.y + 5 >= height) {
-        this.pos.y = height - 5;
-        this.vel.y *= -0.5;
+    if(this.pos.y + this.r >= height) {
+        this.pos.y = height - this.r;
+        this.vel.y *= -0.3;
         this.vel.mult(0.9);
     };
 };
@@ -37,8 +38,10 @@ Scene.prototype.join = function(...args) {
 };
 Scene.prototype.draw = function() {
     stroke(0);
-    strokeWeight(10);
-    this.nodes.forEach(node => point(node.pos.x, node.pos.y));
+    this.nodes.forEach(node => {
+        strokeWeight(node.r * 2);
+        point(node.pos.x, node.pos.y);
+    });
     strokeWeight(3);
     this.springs.forEach(spring => line(spring.p1.pos.x, spring.p1.pos.y, spring.p2.pos.x, spring.p2.pos.y));
 };
@@ -46,9 +49,10 @@ Scene.prototype.update = function() {
     this.springs.forEach(spring => {
         let n1 = spring.p1;
         let n2 = spring.p2;
+        
         let dist = DVector.dist(n1.pos, n2.pos);
         let disp = dist - spring.len;
-        let k = 0.3;
+        let k = 0.5;
         let ang = atan2(n2.pos.y - n1.pos.y, n2.pos.x - n1.pos.x);
 
         n1.vel.x += dt * k * disp * cos(ang);
@@ -64,8 +68,8 @@ Scene.prototype.update = function() {
 var scene = new Scene();
 
 // Why do so many variables just already exist?!
-var point1 = new Node(156, 179);
-var point2 = new Node(426, 300);
+var point1 = new Node(156, 179, 5);
+var point2 = new Node(426, 300, 8);
 
 scene.add(
     point1,

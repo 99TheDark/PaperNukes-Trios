408aaf0fd20f4320194e440cdae18ea3f5882d6f
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-02-19 15:48:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 setCanvas(document.getElementById("game"));
 size(innerWidth, innerHeight);
 
-var Node = function(x, y, r) {
+var Node = function(x, y, static, r) {
     this.pos = new DVector(x, y);
     this.vel = new DVector(0, 0);
-    this.r = r;
-    this.mass = PI * sq(r) * 0.2;
+    this.static = static || false;
+    this.r = r || 5;
+    this.mass = PI * sq(this.r) * 0.2;
 };
 Node.prototype.update = function() {
-    this.vel.y += this.mass * dt;
-    this.vel.mult(0.98)
+    // this.vel.y += this.mass * dt; // gravity
+    this.vel.mult(0.97);
     this.pos.add(this.vel);
 
     if(this.pos.y + this.r >= height) {
@@ -18,8 +19,11 @@ Node.prototype.update = function() {
         this.vel.mult(0.9);
     };
 };
+Node.prototype.reset = function() {
+    this.vel.zero2D();
+};
 
-// TODO: Add tightness
+// TODO: Add tightness (k)
 var Spring = function(p1, p2, len) {
     this.p1 = p1;
     this.p2 = p2;
@@ -37,55 +41,89 @@ Scene.prototype.join = function(...args) {
     args.forEach(spring => this.springs.push(spring));
 };
 Scene.prototype.draw = function() {
+    strokeWeight(3);
+    stroke(210, 0, 0);
+    this.springs.forEach(spring => {
+        line(spring.p1.pos.x, spring.p1.pos.y, spring.p2.pos.x, spring.p2.pos.y);
+    });
     stroke(0);
     this.nodes.forEach(node => {
         strokeWeight(node.r * 2);
         point(node.pos.x, node.pos.y);
     });
-    strokeWeight(3);
-    this.springs.forEach(spring => line(spring.p1.pos.x, spring.p1.pos.y, spring.p2.pos.x, spring.p2.pos.y));
 };
 Scene.prototype.update = function() {
     this.springs.forEach(spring => {
         let n1 = spring.p1;
         let n2 = spring.p2;
-        
+
         let dist = DVector.dist(n1.pos, n2.pos);
         let disp = dist - spring.len;
-        let k = 0.5;
+        let k = 17;
         let ang = atan2(n2.pos.y - n1.pos.y, n2.pos.x - n1.pos.x);
 
-        n1.vel.x += dt * k * disp * cos(ang);
-        n1.vel.y += dt * k * disp * sin(ang);
+        n1.vel.x += dt * k * disp * cos(ang) / n1.mass;
+        n1.vel.y += dt * k * disp * sin(ang) / n1.mass;
 
-        n2.vel.x += dt * k * disp * cos(ang + 180);
-        n2.vel.y += dt * k * disp * sin(ang + 180);
+        n2.vel.x += dt * k * disp * cos(ang + 180) / n2.mass;
+        n2.vel.y += dt * k * disp * sin(ang + 180) / n2.mass;
     });
 
-    this.nodes.forEach(node => node.update());
+    this.nodes.forEach(node => node.static ? node.reset() : node.update());
 };
 
 var scene = new Scene();
 
-// Why do so many variables just already exist?!
-var point1 = new Node(156, 179, 5);
-var point2 = new Node(426, 300, 8);
+var point1 = new Node(0, 0, true);
+var point2 = new Node(100, 0);
+var point3 = new Node(100, 100);
+var point4 = new Node(0, 100);
 
 scene.add(
     point1,
-    point2
+    point2,
+    point3,
+    point4
 );
 
 scene.join(
     new Spring(
         point1,
         point2,
-        150 // sitting length
+        150 // resting length
+    ),
+    new Spring(
+        point2,
+        point3,
+        150
+    ),
+    new Spring(
+        point3,
+        point4,
+        150
+    ),
+    new Spring(
+        point4,
+        point1,
+        150
+    ),
+    new Spring(
+        point1,
+        point3,
+        150 * sqrt(2)
+    ),
+    new Spring(
+        point2,
+        point4,
+        150 * sqrt(2)
     )
 );
 
 draw = function() {
     background(192, 232, 250);
+    pushMatrix();
+    translate(width / 2, height / 2);
     scene.update();
     scene.draw();
+    popMatrix();
 };

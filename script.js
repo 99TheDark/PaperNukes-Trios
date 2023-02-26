ed53800cd0f02eb89a6622fb91444437b5630521
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-02-26 14:10:51 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ size(innerWidth, innerHeight);
 // Globals
 var gravity = true;
 var showVelocities = false;
-var windSpeed = 0.05;
+var windSpeed = 0.07;
+var k = 30;
 
-// For later collisions, might switch
+// For later collisions
 // From https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+// https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment
 var lineCollision = function(x1, y1, x2, y2, x3, y3, x4, y4) {
     let dx1 = x2 - x1;
     let dy1 = y2 - y1;
@@ -18,31 +20,59 @@ var lineCollision = function(x1, y1, x2, y2, x3, y3, x4, y4) {
     let t = (dx2 * (y1 - y3) - dy2 * (x1 - x3)) / (-dx2 * dy1 + dx1 * dy2);
 
     if(s >= 0 && s <= 1 && t >= 0 && t <= 1) {
-        return {
-            "x": x1 + t * dx1,
-            "y": y1 + t * dy1
-        };
+        return new DVector(
+            x1 + t * dx1,
+            y1 + t * dy1
+        );
     }
     return null;
+};
+var shortestDistance = function(p, q, n) {
+    var len2 = pow(DVector.dist(p, q), 2);
+    if(len2 === 0) {
+        return DVector.dist(p, n);
+    }
+    var diff = DVector.sub(q, p);
+    var dot = DVector.dot(DVector.sub(n, p), diff);
+    var t = constrain(dot / len2, 0, 1);
+    diff.mult(t);
+    var proj = DVector.add(p, diff);
+    return {
+        "proj": proj,
+        "dist": DVector.dist(n, proj)
+    };
 };
 
 var Node = function(x, y, static, r) {
     this.pos = new DVector(x, y + height / 2);
-    this.vel = new DVector(this.pos.y / 6 - 70, -20); // give rotational energy, not final
+    this.lastPos = new DVector(this.pos.x, this.pos.y);
+    this.vel = new DVector((this.pos.y / 8 - 70) * 0, -20); // give rotational energy, not final
     this.static = static || false;
     this.r = r || 6;
     this.mass = PI * sq(this.r) * 0.3;
 };
 Node.prototype.update = function() {
+    [this.lastPos.x, this.lastPos.y] = [this.pos.x, this.pos.y];
+
     if(gravity) this.vel.y += this.mass * dt; // gravity
     this.vel.mult(0.97);
     this.pos.add(this.vel);
 
     if(this.pos.y + this.r >= height) {
         this.pos.y = height - this.r;
-        this.vel.y *= -0.3;
-        this.vel.mult(0.9);
-    };
+        this.vel.y *= -0.7;
+        this.vel.mult(0.85);
+    }
+    /*if(this.pos.x + this.r >= width) {
+        this.pos.x = width - this.r;
+        this.vel.x *= -0.7;
+        this.vel.mult(0.85);
+    }
+    if(this.pos.x - this.r <= 0) {
+        this.pos.x = this.r;
+        this.vel.x *= -0.7;
+        this.vel.mult(0.85);
+    }*/
 };
 Node.prototype.reset = function() {
     this.vel.zero2D();
@@ -51,6 +81,44 @@ Node.prototype.copy = function() {
     let n = new Node(this.pos.x, this.pos.y, this.static, this.r);
     n.vel.set(this.vel);
     return n;
+};
+Node.prototype.collideSpring = function(spring) {
+    let collision = lineCollision(
+        spring.p1.pos.x,
+        spring.p1.pos.y,
+        spring.p2.pos.x,
+        spring.p2.pos.y,
+        this.lastPos.x,
+        this.lastPos.y,
+        this.pos.x,
+        this.pos.y
+    );
+
+    if(collision && spring.p1 != this && spring.p2 != this) {
+        let short = shortestDistance(spring.p1.pos, spring.p2.pos, this.lastPos);
+
+        let cpos = DVector.sub(
+            collision,
+            DVector.mult(
+                DVector.mult(
+                    DVector.normalize(this.vel),
+                    DVector.dist(collision, this.lastPos) / short.dist
+                ), 
+                this.r
+            )
+        );
+        let v = DVector.sub(cpos, this.pos);
+
+        let dpos = DVector.sub(cpos, this.pos);
+        this.pos.add(dpos);
+        this.vel.add(v);
+
+        spring.p1.vel.sub(v);
+        spring.p2.vel.sub(v);
+        
+        this.mesh.nodes.forEach(node => node.vel.add(DVector.mult(v, 1.3)));
+        spring.mesh.nodes.forEach(node => node.vel.sub(DVector.mult(v, 1.3)));
+    }
 };
 
 var Spring = function(p1, p2, len, i1, i2) {
@@ -63,8 +131,7 @@ var Spring = function(p1, p2, len, i1, i2) {
     this.i2 = i2;
 };
 Spring.prototype.update = function() {
-    this.disp =  DVector.dist(this.p1.pos, this.p2.pos) - this.len;
-    let k = 70;
+    this.disp = DVector.dist(this.p1.pos, this.p2.pos) - this.len;
     let ang = atan2(this.p2.pos.y - this.p1.pos.y, this.p2.pos.x - this.p1.pos.x);
 
     this.p1.vel.x += dt * k * this.disp * cos(ang) / this.p1.mass;
@@ -74,40 +141,57 @@ Spring.prototype.update = function() {
     this.p2.vel.y += dt * k * this.disp * sin(ang + 180) / this.p2.mass;
 };
 
-var Scene = function() {
+var Mesh = function() {
     this.nodes = [];
     this.springs = [];
 };
-Scene.prototype.add = function(...args) {
-    args.forEach(node => this.nodes.push(node));
+Mesh.prototype.add = function(...args) {
+    args.forEach(node => this.nodes.push(node) & (node.mesh = this));
 };
-Scene.prototype.join = function(...args) {
-    args.forEach(spring => this.springs.push(spring));
+Mesh.prototype.join = function(...args) {
+    args.forEach(spring => this.springs.push(spring) & (spring.mesh = this));
+};
+
+var Scene = function() {
+    this.objs = [];
+};
+Scene.prototype.add = function(mesh) {
+    this.objs.push(mesh);
 };
 Scene.prototype.draw = function() {
-    strokeWeight(3);
-    this.springs.forEach(spring => {
-        let col = lerpColor(color(255, 0, 0), color(0, 0, 0), abs(spring.disp) / 40);
-        stroke(red(col), green(col), blue(col), alpha(col)); // gotta fix this glitch in Dark.js
-        line(spring.p1.pos.x, spring.p1.pos.y, spring.p2.pos.x, spring.p2.pos.y);
-    });
-    let m = 10;
-    this.nodes.forEach(node => {
-        stroke(0);
-        strokeWeight(node.r * 2);
-        point(node.pos.x, node.pos.y);
+    this.objs.forEach(obj => {
+        strokeWeight(3);
+        obj.springs.forEach(spring => {
+            let col = lerpColor(color(255, 0, 0), color(0, 0, 0), abs(spring.disp) / 40);
+            stroke(red(col), green(col), blue(col), alpha(col)); // gotta fix this glitch in Dark.js
+            line(spring.p1.pos.x, spring.p1.pos.y, spring.p2.pos.x, spring.p2.pos.y);
+        });
+        let m = 10;
+        obj.nodes.forEach(node => {
+            stroke(0);
+            strokeWeight(node.r * 2);
+            point(node.pos.x, node.pos.y);
 
-        if(showVelocities) {
-            stroke(0, 150, 0);
-            strokeWeight(3);
-            line(node.pos.x, node.pos.y, node.pos.x + node.vel.x * m, node.pos.y + node.vel.y * m);
-        }
+            if(showVelocities) {
+                stroke(0, 150, 0);
+                strokeWeight(3);
+                line(node.pos.x, node.pos.y, node.pos.x + node.vel.x * m, node.pos.y + node.vel.y * m);
+            }
+        });
     });
 };
 Scene.prototype.update = function() {
-    this.springs.forEach(spring => spring.update());
-
-    this.nodes.forEach(node => (node.vel.x += windSpeed) && node.static ? node.reset() : node.update());
+    this.objs.forEach(obj => {
+        obj.springs.forEach(spring => spring.update());
+        obj.nodes.forEach(node => (node.vel.x += windSpeed) && node.static ? node.reset() : node.update());
+    });
+    this.objs.forEach(obj => {
+        this.objs.forEach(collisionObj => {
+            if(collisionObj != obj) {
+                obj.nodes.forEach(node => collisionObj.springs.forEach(spring => node.collideSpring(spring)));
+            }
+        });
+    });
 };
 
 var loadLevel = function(txt) {
@@ -181,14 +265,24 @@ var loadLevel = function(txt) {
                     case "push":
                         let velocity = new DVector(Number(obj[2]), Number(obj[3]));
                         curObj.nodes.forEach(node => node.vel.add(velocity));
+                        break;
+                    case "scale":
+                        let scaleVal = Number(obj[2]);
+                        curObj.nodes.forEach(node => node.pos.mult(scaleVal));
+                        curObj.springs.forEach(spring => spring.len *= scaleVal);
+                        break;
                 }
                 break;
         }
     });
 
     objs.forEach(obj => {
-        scene.add.apply(scene, obj.nodes);
-        scene.join.apply(scene, obj.springs);
+        let mesh = new Mesh();
+        mesh.add.apply(mesh, obj.nodes),
+            mesh.join.apply(mesh, obj.springs);
+        scene.add(
+            mesh
+        );
     });
 };
 
